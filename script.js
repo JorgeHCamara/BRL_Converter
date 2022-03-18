666a2f567f5e78152a4bcb4bcc06ac3f8f461713
JorgeHCamara <jorgehcamara@hotmail.com>
2022-03-17 21:18:11 -0300

    Other currencies JS

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function converter() {
     let brl = inputBRL.value;
 
     let brlDollar = brl / 5;
+    let brlEuro = brl / 6;
+    let brlPound = brl / 7;
+    let brlCad = brl / 4;
 
-    conversions.innerHTML = `<p>US$${brlDollar}</p>`
+    conversions.innerHTML = `<p>US Dollar: US$${Math.ceil(brlDollar)}</p>`;
+    conversions.innerHTML += `<p>Euro: €${Math.ceil(brlEuro)}</p>`;
+    conversions.innerHTML += `<p>British Pound: £${Math.ceil(brlPound)}</p>`;
+    conversions.innerHTML += `<p>Canadian Dollar: CAD$${Math.ceil(brlCad)}</p>`;
 }
